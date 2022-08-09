719f2780fa2cf9556d4e8c359102c5783184411d
logic <logicfeel@nate.com>
2022-08-09 18:03:59 +0900

    파일 로딩

--- src/automation.js
+++ src/automation.js
@@ -1,4 +1,8 @@
 
+const fs = require('fs');
+const path = require('path');
+const {MetaElement, PropertyCollection, MetaObject} = require('entitybind');
+
 class Automation {
     
     // private
@@ -7,6 +11,10 @@ class Automation {
     _install = null;
     _resolve = null;
     _auto = null;
+    _package = null;
+
+    // public
+    mod = new AutoCollection(this);
 
     constructor(dir) {
         console.log('Automation load..')
@@ -17,15 +25,113 @@ class Automation {
         let installPath;
         let resolvePath;
         let autoPath;
+        let packagePath;
 
         installPath = this._dir + '/install.json';
         resolvePath = this._dir + '/resolve.json';
-        autoPath = this._dir + '/auto.json';
+        packagePath = this._dir + '/package.json';
 
-        // 파일검사 !!
-        this._install = require(installPath);
+        // 파일검사
+        // 선ㅐ
+        if (fs.existsSync(installPath)) this._install = require(installPath);
+        if (fs.existsSync(resolvePath)) this._resolve = require(resolvePath);
+        if (fs.existsSync(autoPath)) this._auto = require(autoPath);
+        // 필수
+        if (!fs.existsSync(packagePath)) throw new Error('package.json file fail...');
+        this._package = require(packagePath);
+        
+        this.name = this._package.name;
     }
 }
 
+
+
+class AutoCollection extends PropertyCollection {
+    constructor(onwer) {
+        super(onwer);
+        
+        this._super = [];
+        this._sub = [];
+    }
+    
+    _getSuperList() {
+        
+        let arr = [];
+        let elm;
+        let obj;
+
+        for(let i = 0; i < this._super.length; i++) {
+            elm = this[this._super[i]];
+            if (elm.mod._super.length > 0) {
+                arr = arr.concat(elm.mod._getSuperList());
+            }
+            obj = {
+                key: `${elm.package.name}.${this._super[i]}`,
+                value: elm
+            };
+            arr.push(obj);
+        }
+        return arr;
+    }
+
+    getObject(p_context) {
+
+        let obj     = {};
+
+        for (let prop in this) {
+            if (this[prop] instanceof MetaElement) {
+                obj[prop] = this[prop].getObject(p_context);
+            } else if (prop.substring(0, 1) !== '_') {
+                obj[prop] = this[prop];
+            }
+        }
+        return obj;                      
+    }
+
+
+    getDependList() {
+        
+        let arr = [];
+        let elm;
+        let obj;
+
+        for(let i = 0; i < this._sub.length; i++) {
+            elm = this[this._sub[i]];
+            obj = {
+                key: `${elm.package.name}.${this._sub[i]}`,
+                value: elm
+            }
+            arr.push(obj);
+        }
+        arr = arr.concat(this._getSuperList());
+        return arr;
+    }
+
+    add(alias, obj) {
+        super.add(alias, obj);
+    }
+    
+    sub(alias, obj) {
+        this.add(alias, obj);
+        // 별칭 이름 등록
+        this._sub.push(alias);
+        // 의존 모듈 등록
+        // this._onwer.dep[`${obj.package.name}.${alias}`] = obj;
+    }
+    
+    super(alias, obj) {
+        this.add(alias, obj);
+        // 별칭 이름 등록
+        this._super.push(alias);
+        // 의존 모듈 등록  하위로 
+        // this._onwer.dep[`${obj.package.name}.${alias}`] = obj;
+        // 
+    }
+
+    select(seloector) {}
+
+    select(seloector, obj) {}
+   
+}
 
 module.exports = Automation;
