0c0d520a0d5319046865f331d24d97d3a8db24d0
logicfeel <logicfeel@nate.com>
2022-08-10 17:39:36 +0900

    개발중

--- src/auto-task.js
+++ src/auto-task.js
@@ -22,12 +22,14 @@ class AutoTask {
         console.log('_load()....');
         // 현재 폴더의 auto.js 파일 로딩
         let entryFile  = this.__dirname + '/auto.js'
+        // 다양한 조건에 예외조건을 수용해야함
         const EntryAuto = require(entryFile);
         // 타입 검사해야함
         this.entry = new EntryAuto();
     }
 
     do_dist() {
+        // 로딩
         this._load();
     }
 }
