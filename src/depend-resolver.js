7e80ce8c67612e51719c3c717bd9f8c66c0f1931
logic <logicfeel@nate.com>
2022-08-26 17:52:39 +0900

    resolver 1차 성공

--- src/depend-resolver.js
+++ src/depend-resolver.js
@@ -17,14 +17,40 @@ class DependResolver {
         this._getRefer();
     }
 
-    __getPath(src, location, alias = '') {
-        // 임시 객체
-        let objPath = {
-            location: location,
-            alias: alias,
-            src: src
-        };    
-        return objPath;
+    // 의존성 처리
+    resolve() {
+        
+        let arr, target, content, keyword;
+        let indexArr = [];
+
+        // 내부함수
+        function getRef(target, list) {
+            let refObj = {
+                target: target,
+                list: list
+            };
+            return refObj;
+        }
+
+        // 전체 소스 조회
+        for (let i = 0; i < this._org.length; i++) {
+            arr = this.__getReferPath(this._org[i]);
+            content = this._org[i].src.content;
+            // 참조 대상 조회
+            for (let ii = 0; ii < arr.length; ii++) {
+                // 대상의 상대, 절대 경로
+                indexArr = [];  // 초기화
+                target = arr[ii].src;
+                for (let iii = 0; iii < arr[ii].path.length; iii++) {
+                    keyword = arr[ii].path[iii];    
+                    indexArr = indexArr.concat(this.__searchPath(keyword, content));    // 벼열 합침
+                }
+                // 참조가 있으면 등록
+                if (indexArr.length > 0) {
+                    this._org[i].src._ref.push(getRef(target, indexArr));
+                }                
+            }
+        }
     }
 
     // 해결대상 가져오기
@@ -59,15 +85,25 @@ class DependResolver {
         }
     }
 
+    __getPath(src, location, alias = '') {
+        // 임시 객체
+        let objPath = {
+            location: location,
+            alias: alias,
+            src: src
+        };    
+        return objPath;
+    }
+
     // 소스에 대한 참조 목록 가져오기
     __getReferPath(org) {
         
         let arr = [];
         let relativePath = null;
-        let src;
+        let src, dir;
 
         // 내부함수
-        function getKey(src, keyPath) {
+        function getKey(src, ...keyPath) {
             let key = {
                 path: keyPath,
                 src: src
@@ -76,64 +112,64 @@ class DependResolver {
         }
 
         if (org.location === 'src') {
-            for (let i = 0; this._ref.length; i++) {
+            for (let i = 0; i < this._ref.length; i++) {
                 src = this._ref[i].src;
                 if (['src', 'out'].indexOf(this._ref[i].location) > -1) {
                     // 절대경로 추가
-                    arr.push(getKey(src, src.basePath));
+                    // arr.push(getKey(src, src.basePath));
                     // 상대경로 추가
-                    relativePath = path.relative(org.src.fullPath, src.fullPath);
-                    arr.push(getKey(src, relativePath));
+                    dir = path.dirname(org.src.fullPath);
+                    relativePath = path.relative(dir, src.fullPath);
+                    arr.push(getKey(src, src.basePath, relativePath));
                 } else if (this._ref[i].location === 'dep') {
                     // TODO:: 별칭 정보를 확인해서 삽입
                 }
             }
         } else if (org.location === 'out') {
-            for (let i = 0; this._ref.length; i++) {
+            for (let i = 0; i < this._ref.length; i++) {
                 src = this._ref[i].src;
                 if (['out'].indexOf(this._ref[i].location) > -1) {
                     // 절대경로 추가
-                    arr.push(getKey(src, src.basePath));
+                    // arr.push(getKey(src, src.basePath));
                     // 상대경로 추가
-                    relativePath = path.relative(org.fullPath, src.fullPath);
-                    arr.push(getKey(src, relativePath));
+                    dir = path.dirname(org.src.fullPath);
+                    relativePath = path.relative(dir, src.fullPath);
+                    arr.push(getKey(src, src.basePath, relativePath));
                 }
             }
         }
         return arr;
     }
 
-    // 의존성 처리
-    resolve() {
+
+
+    __searchPath(pathKey, cnt) {
         
-        let arr, target, content, index, keyword;
-
-        // 내부함수
-        function getRef(target, index, length) {
-            let refObj = {
-                target: keyPath,
-                index: src,
-                length: length
-            };
-            return refObj;
+        let reg
+        let rtnArr = [];
+        let str = pathKey;
+        
+        str = str.replaceAll('\\', "\\\\");
+        //str = str.replaceAll('/', '\/');
+        str = str.replaceAll('.', '\\.');
+    
+        str =  "(?:[^\\w\\\\/\\.])" + str + "(?:[^\\w\\\\/\\.])";
+        
+        reg = RegExp(str, 'gi');
+    
+        while(reg.exec(cnt)) {
+            if (reg.lastIndex === 0) break;
+            rtnArr.push({
+                idx: reg.lastIndex - str.length - 1,
+                key: pathKey
+            });
+            console.log(reg.lastIndex)
         }
-
-        // 전체 소스 조회
-        for (let i = 0; this._org.length; i++) {
-            arr = this.__getReferPath(this._org[i]);
-            content = this._org[i].src.content;
-
-            for (let ii = 0; arr.length; ii++) {
-                target = his._org[i].src;
-                keyword = arr[ii];
-                index = conent.indexOf(keyword);
-                // 참조 조회
-                if (index > -1) {
-                    this._org[i].src._ref.push(refObj(target, index, keyword.length));
-                }
-            }
-        }
+    
+        return rtnArr;
     }
+
+    
 }
 
 module.exports = DependResolver;
--- task/M1/dist_test.js
+++ task/M1/dist_test.js
@@ -5,4 +5,39 @@ const autoTask = require('../../src/auto-task').getInstance(__dirname);
 
 autoTask.do_dist();
 
+var text = `
+@import url("../../out/area/top.css");
+
+p {
+    font-family: verdana;
+    font-size: 20px;
+    background-color: rgb(176, 230, 173);
+}
+
+/*
+    url("../../out/area/top.css");
+
+    /Users/kimneo/PJ-Git/auto/task/m1/out/area/top.css
+
+    /Users/kimneo/PJ-Git/auto/task/m1/src/inc/left.css
+
+*/
+`;
+
+// var r = _searchPath("../../out/area/top.css", text);
+
+// var rst
+
+// rst = r.exec(text)
+
+// console.log(r.lastIndex)
+
+// rst = r.exec(text)
+
+// console.log(r.lastIndex)
+
+// rst = r.exec(text)
+
+// console.log(r.lastIndex)
+
 console.log(1)
